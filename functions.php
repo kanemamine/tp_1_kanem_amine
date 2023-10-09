d08989545856eff872b27ae5e1304a74240a7b29
Amine Kanem <aminekanem9@gmail.com>
2023-10-09 18:29:00 -0400

    V 1.3

--- functions.php
+++ functions.php
@@ -12,17 +12,26 @@ if ($_POST) {
 
     // Vérifie si la variable $data est vide
     if (!$data) {
-        echo "T'attends de moi quoi au juste !!?";
+        echo 
+        "<div style='margin-bottom: 20px; font-family: Arial, sans-serif; background-color: #f9f9f9; padding: 20px; border-radius: 10px; box-shadow: 0 0 10px rgba(0, 0, 0, 0.1);'>
+                <p style='font-size: 18px;'><strong>T'attends de moi quoi au juste !!?</p>
+        </div>";
     } else if (strlen($data) < 6 ) {
         // Vérifie si la longueur de $data est inférieure à 6 caractères
-        echo "il me faudrait une loupe pour voir votre mot de passe ! un petit effort svp.";
+        echo 
+        "<div style='margin-bottom: 20px; font-family: Arial, sans-serif; background-color: #f9f9f9; padding: 20px; border-radius: 10px; box-shadow: 0 0 10px rgba(0, 0, 0, 0.1);'>
+            <p style='font-size: 18px;'><strong>il me faudrait une loupe pour voir votre mot de passe ! un petit effort svp.</p>
+        </div>";
     } else if (strlen($data) > 10) {
         // Vérifie si la longueur de $data est supérieure à 10 caractères
-        echo "Votre mot de passe est un véritable chef-d'œuvre littéraire, mais nous avons juste besoin d'un mot de passe";
+        echo 
+        "<div style='margin-bottom: 20px; font-family: Arial, sans-serif; background-color: #f9f9f9; padding: 20px; border-radius: 10px; box-shadow: 0 0 10px rgba(0, 0, 0, 0.1);'>
+        <p style='font-size: 18px;'><strong>Votre mot de passe est un véritable chef-d'œuvre littéraire, mais nous avons juste besoin d'un mot de passe</p>
+        </div>";
+        
     } else {
         // Appelle la fonction addSalt pour ajouter du sel au mot de passe
         $dataS = addSalt($data);
-        
         // Affiche le resultat 
         echo '
         <div style="font-family: Arial, sans-serif; background-color: #f9f9f9; padding: 20px; border-radius: 10px; box-shadow: 0 0 10px rgba(0, 0, 0, 0.1);">
